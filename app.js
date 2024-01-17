a17b6fbf8910c5a02d2e74cc5732462cfc1cfbb5
Pavo1034 <kumarpawan963258@gmail.com>
2024-01-17 19:15:22 +0530

    updated

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const heading = React.createElement("div",{id:"parent" ,abc:"xyz"},
-[React.createElement("div",{id:"child1" ,abc:"xyz"},[React.createElement("h1",{id:"heading1" ,abc:"xyz"},"i am h1 tag"),React.createElement("h2",{id:"heading2" ,abc:"xyz"},"i am h2 tag")]),
+[React.createElement("div",{id:"child1" ,abc:"xyz"},[React.createElement("h1",{id:"heading1" ,abc:"xyz"},"i am h1 tag "),React.createElement("h2",{id:"heading2" ,abc:"xyz"},"i am h2 tag")]),
 React.createElement("div",{id:"child2" ,abc:"xyz"},[React.createElement("h1",{id:"heading3" ,abc:"xyz"},"i am h1 tag"),React.createElement("h2",{id:"heading4" ,abc:"xyz"},"i am h2 tag")])])
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(heading)
